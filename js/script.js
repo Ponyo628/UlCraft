60109b0bd3c70008a38bd1c91e99fc6e566685fa
Christina <80633488+Ponyo628@users.noreply.github.com>
2021-03-14 17:39:04 +0300

    new site

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".slider").slick({
+  	arrows:true, 
+  	dots: false,
+  	adaptiveHeight: false,
+  	speed: 900,
+  	easing: "_ease_",
+  	autoplay: true,
+  	autoplaySpeed: 3000,
+  	pauseOnHover: true,
+  	pauseOnFocus: true,
+  	touchThreshold: 12,
+
+  });
+});
